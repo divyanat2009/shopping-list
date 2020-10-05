90a6204370e05966267d742a01ad44e0212003f6
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-05 15:08:40 -0500

    added js file.

---
+++ index.js
@@ -0,0 +1,10 @@
+
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+
+    });
+    $('.shopping-item-controls').click(function(event){
+        $('.shopping-item-toggle').toggle()
+    })
+
+
