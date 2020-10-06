24f8e3b0ee6add550557ada9b28277319c651798
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-06 12:07:04 -0500

    created funntion to add items

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-
+function addItem(){
     $('#js-shopping-list-form').submit(function(event){
         event.preventDefault();
-
-    });
-    $('.shopping-item-controls').click(function(event){
-        $('.shopping-item-toggle').toggle()
+        console.log("Hello World");
+        const entryItem=$('#shopping-list-entry').val()
+        const liItem=`<li>
+        <span class="shopping-item">${entryItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`;
+      $('.shopping-list').append(liItem)
+    
     })
+}
+function checkItem(){
+    $('.shopping-item-toggle').click(function(){
+        console.log($(".shopping-item").find('data-id').text());
+        $('.shopping-item').addClass('shopping-item__checked')
+    });
+}
+function init(){
+    addItem();
+    checkItem();
+}    
+(function(){
+   /* $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+    });
 
+
+    
+    $('.shopping-list-entry').on('click', '.shopping-item-toggle', function(event){
+        $(this).closest('.shopping-item-controls').toggle('.shopping-item-toggle')
+    });
+*/
+init();
+
+})();
 
